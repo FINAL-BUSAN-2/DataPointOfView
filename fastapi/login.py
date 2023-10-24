6ebed83285f20a49dc8a28dd24400626b831ec0a
qwert0175 <suin990209@gmail.com>
2023-10-24 13:08:10 +0900

    [FNT] 카메라 권한 받기 #146

--- fastapi/login.py
+++ fastapi/login.py
@@ -88,7 +88,13 @@ async def kakao_callback(code: str, request: Request, db: Session = Depends(get_
 
     async with httpx.AsyncClient() as client:
         response = await client.post(token_endpoint, data=data)
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=400, detail="Error requesting access token from Kakao")
+
         token_data = response.json()
+        if "error" in token_data:
+            raise HTTPException(status_code=400, detail=token_data["error_description"])
 
     # Kakao 사용자 정보 요청
     user_info_endpoint = "https://kapi.kakao.com/v2/user/me"
@@ -98,7 +104,13 @@ async def kakao_callback(code: str, request: Request, db: Session = Depends(get_
 
     async with httpx.AsyncClient() as client:
         response = await client.get(user_info_endpoint, headers=headers)
+
+        if response.status_code != 200:
+            raise HTTPException(status_code=400, detail="Error requesting user information from Kakao")
+
         user_info = response.json()
+        if "error" in user_info:
+            raise HTTPException(status_code=400, detail=user_info["error_description"])
     
     now = datetime.now()
     sday = now.date()
