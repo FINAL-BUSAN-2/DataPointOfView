6015515c32de1675292237fe74636359e9e40ef8
Minji Ji <145173900+JIMINJI1@users.noreply.github.com>
2023-10-26 07:11:48 +0900

    #168 검색창수정

--- reactnative/serch.tsx
+++ reactnative/serch.tsx
@@ -57,11 +57,34 @@ function Search() {
 
   return (
     // 검색창
-    <View style={styles.container}>
-      <TextInput
-        value={keyword}
-        onChange={onChangeData}
-        style={styles.search}
+    <View style={{flex: 1}}>
+      <View style={styles.container}>
+        <TextInput
+          value={keyword}
+          onChange={onChangeData}
+          style={styles.search}
+          placeholder="검색어 입력"
+        />
+        {/* 취소 버튼 */}
+        <TouchableOpacity
+          style={styles.cancelButton}
+          onPress={() => {
+            //setKeyword(''); 검색내역지우기
+            //setKeyItems([]);
+            navigation.goBack(); //뒤로가기
+          }}>
+          <Text style={styles.cancelButtonText}>취소</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={{marginTop: 10}}></View>
+      {/* 실선 */}
+      <View
+        style={{
+          borderTopWidth: 1,
+          borderTopColor: 'rgb(175, 171, 171)',
+          width: '100%', // 화면 전체 너비에 맞춤
+        }}
       />
 
       {keyItems.length > 0 && keyword && (
@@ -93,23 +116,32 @@ function Search() {
 const styles = StyleSheet.create({
   //검색창박스
   container: {
-    width: 300,
+    marginTop: 20, //위로부터 띄우기
+    marginHorizontal: 20, //양옆띄우기
+    width: 380,
     height: 60, // 높이 값을 조금 더 크게 설정
     position: 'relative',
-    borderWidth: 1,
-    borderColor: 'black',
+    flexDirection: 'row', // 방향 설정
+    alignItems: 'center', // 세로 정렬
+    justifyContent: 'space-between', // 가로 정렬
+
+    //borderWidth: 1,
+    //borderColor: 'black',
   },
   //검색창
   search: {
+    flex: 1, // 검색창이 가능한 한 많은 공간을 차지하도록
     paddingLeft: 15,
     paddingRight: 15,
-    backgroundColor: 'pink', //검색창 색상
-    width: 300,
+    backgroundColor: 'rgb(231,230,230)', //검색창 색상
+    width: 330,
     height: 60, // 전체 부모 컨테이너의 높이를 차지하게 설정
     color: '#333', //입력되는글자색상
     fontSize: 14, // fontSize 값을 조금 줄임
     paddingVertical: 10, // paddingVertical 값을 조절
     borderRadius: 10, // 라운드 모서리 추가
+    marginBottom: 10, // 간격 추가
+    marginRight: 10, // 취소 버튼과의 간격
   },
   searchIcon: {
     position: 'absolute',
@@ -119,12 +151,13 @@ const styles = StyleSheet.create({
   //연관검색창
   autoSearchContainer: {
     position: 'absolute',
-    top: 50,
+    top: 80,
     maxHeight: 200, // 높이를 제한
-    width: 300,
+    width: 320,
     backgroundColor: '#fff',
     padding: 15,
-    borderWidth: 2,
+    //borderWidth: 2,
+    marginHorizontal: 20,
   },
   item: {
     padding: 10,
@@ -135,6 +168,13 @@ const styles = StyleSheet.create({
   arrowIcon: {
     width: 18,
     height: 18,
+  },
+  cancelButton: {
+    padding: 10,
+  },
+  cancelButtonText: {
+    color: 'black', // 일반적으로 취소 버튼의 색상
+    //fontSize:
   },
 });
 
